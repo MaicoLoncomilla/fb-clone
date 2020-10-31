603549ad2961f2e86b9ea2ef6eb68b9a76502c54
Maico <maicoloncomilla@gmail.com>
2020-10-31 20:27:26 -0300

    Updates

---
+++ precache-manifest.98bdc1439607c018d8ac6939a2c7726d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc5d1f62db2162b25482242c9e085fc",
+    "url": "/fb-clone/index.html"
+  },
+  {
+    "revision": "d112d780e872dd44764e",
+    "url": "/fb-clone/static/css/main.d8f33193.chunk.css"
+  },
+  {
+    "revision": "6efb4fd7e82aa9641007",
+    "url": "/fb-clone/static/js/2.0fabae65.chunk.js"
+  },
+  {
+    "revision": "1a357c5bc31d844a6f7dae853de2d673",
+    "url": "/fb-clone/static/js/2.0fabae65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d112d780e872dd44764e",
+    "url": "/fb-clone/static/js/main.65e9b6d7.chunk.js"
+  },
+  {
+    "revision": "70210a7fd678a62291cb",
+    "url": "/fb-clone/static/js/runtime-main.98fca3d7.js"
+  }
+]);
